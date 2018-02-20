970d2db606b5db3c581072712d7ccc048194fe1b
Jeremy Sarachan <jsarachan@sjfc.edu>
2018-02-19 23:19:06 -0500

    Add files via upload

---
+++ SlideOutBar/jQuery_EX3.js
@@ -0,0 +1,62 @@
+$(function(){
+  //for accordion
+    
+  var accordion = $('.accordion');
+  var headings = $('main h2');
+  var paragraphs = $('main p');
+    var openPort=false;
+  //hide all paragraphs in the accordion
+  paragraphs.hide();
+  
+  accordion.on('click','h2',function(){
+      var t = $(this);
+      var tPara = t.next();
+      if (!tPara.is(':visible')){
+          tPara.slideDown('normal');
+      } else {
+           tPara.slideUp('normal');
+      }
+ })
+      
+    
+      
+//for tabs
+    var clickedOnTab = $('ul.tabs');
+	clickedOnTab.on('click','li',function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+
+      });
+    
+// for slideout
+    
+$("body").on("mousemove",function(event) {
+    if (event.pageX < 100 && openPort==true) {
+        $('.portfolio').animate({"width":200},{duration:500, easing:"easeInOutQuad", complete:openPort=false});
+        $('.portfolio').removeClass('hideScroll');
+        /*$('#wrapper').animate({"margin-left":200},500);*/
+            
+        } 
+     });
+    
+$("body").on("mousemove",function(event) {
+    if (event.pageX >250 && openPort==false) {
+        $('.portfolio').animate({"width":-20},{duration:500, easing:"swing", complete:openPort=true});
+        $('.portfolio').addClass('hideScroll');
+        /*$('#wrapper').animate({"margin-left":0},500);*/
+            
+        } 
+     });
+    
+    
+});
+    
+    
+    
+    
+  
